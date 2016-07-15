2fa82e8904a164fe8bdc7abbd34af1ade59b942e
victor Rojas <victor.rojas@ec.pe>
2016-07-15 16:42:15 -0500

    cvambio

--- Products_files/copiar.producto.js
+++ Products_files/copiar.producto.js
@@ -1,32 +1,79 @@
-/**
- * Created by victor on 15/07/16.
- */
-//select Tienda
-var selectTienda = document.querySelector(".testSelect");
-var selectOption = selectTienda.options[selectTienda.selectedIndex];
-var lastSelected = localStorage.getItem('testSelect');
+(function() {
 
-if (lastSelected) {
-    selectTienda.value = lastSelected;
-}
+    var $hostname = window.location.host;
+    var selectNameTienda = '.testSelect';
+    var selectNameCatalogo = '.SelectCatalogo';
+    var urlServiceCatalogo = 'http://dev.3.elcomercio.pe/catalogo/api/catalogo';
+    var urlServiceTienda = 'http://dev.3.elcomercio.pe/catalogo/api/tienda';
 
-selectTienda.onchange = function () {
-    lastSelected = selectTienda.options[selectTienda.selectedIndex].value;
-    console.log(lastSelected);
-    localStorage.setItem('testSelect', lastSelected);
-}
-// select Catalogo
-var select = document.querySelector(".SelectCatalogo");
-var selectOption = select.options[select.selectedIndex];
-var lastSelected = localStorage.getItem('SelectCatalogo');
+    var copy = {
+        init : function (selectName) {
+            var selectTienda = document.querySelector(selectName);
+            var selectOption = selectTienda.options[selectTienda.selectedIndex];
+            var lastSelected = localStorage.getItem(selectName);
 
-if (lastSelected) {
-    select.value = lastSelected;
-}
+            if (lastSelected) {
+                selectTienda.value = lastSelected;
+            }
 
-select.onchange = function () {
-    lastSelected = select.options[select.selectedIndex].value;
-    console.log(lastSelected);
-    localStorage.setItem('SelectCatalogo', lastSelected);
-}
-//http://stackoverflow.com/questions/23905358/how-to-use-localstorage-on-last-html-select-value
+            selectTienda.onchange = function () {
+                lastSelected = selectTienda.options[selectTienda.selectedIndex].value;
+                console.log(lastSelected);
+                localStorage.setItem(selectName, lastSelected);
+            }
+        },
+        //getEnviroment : function (){
+        //
+        //    var $indexOf_hostname = $hostname.indexOf("dev.catalogo.ecincubator.net");
+        //
+        //    if ($indexOf_hostname != -1){
+        //        switch ($hostname.substr(0, $indexOf_hostname)){
+        //            case "dev.3." : return 'dev.3.';
+        //                break;
+        //            case "pre.3." : return 'pre.3.';
+        //                break;
+        //            default :
+        //                USERNAME = "service_hiraoka@gmail.com";
+        //                PASSWORD = "Cd3v3l0per*1";
+        //                return '';
+        //                break;
+        //        }
+        //    }
+        //
+        //},
+        getJsonP: function (urlService,callback){
+            $.ajax({
+                url: urlService,
+                jsonp: "callback",
+                dataType: 'jsonp',
+                type: 'GET',
+                error: function(error) {
+                    console.log(error);
+                }
+            }).done(function(response) {
+                if (response.success) {
+                    var dataCatalogo = response.data;
+                    callback(dataCatalogo);
+                }
+            });
+        }
+    }
+
+    copy.getJsonP(urlServiceCatalogo,function(dataCatalogo){
+        var $select = $(selectNameCatalogo);
+        $.each(dataCatalogo, function(i, val){
+            $select.append($('<option />', { value: (val.catalogo_id), text: val.nombre }));
+        });
+        copy.init(selectNameCatalogo);
+    });
+
+    copy.getJsonP(urlServiceTienda,function(dataTienda){
+        var $select = $(selectNameTienda);
+        console.log(dataTienda);
+        $.each(dataTienda, function(i, val){
+            $select.append($('<option />', { value: (val.empresa_id), text: val.razonsocial }));
+        });
+        copy.init(selectNameTienda);
+    });
+    
+}());
